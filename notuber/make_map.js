bdb4065f17fc667680155b316608b3f0f9548be0
georgewest13 <gwest2016@spyponders.com>
2018-03-04 19:19:40 -0500

    Functioning API

--- notuber/make_map.js
+++ notuber/make_map.js
@@ -2,19 +2,18 @@ var myLat = 0;
 var myLng = 0;
 var me = new google.maps.LatLng(myLat, myLng);
 var myOptions = {
-	zoom: 13, // The larger the zoom number, the bigger the zoom
+	zoom: 16, // The larger the zoom number, the bigger the zoom
 	center: me,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map;
 var marker;
 var infowindow = new google.maps.InfoWindow();
-var drivers;
+var shortDist = 6378137 * 0.000621371; 
 
 function init() {
 	map = new google.maps.Map(document.getElementById("map"), myOptions);
-	getMyLocation();
-	getCarsLocations();
+	getMyLocation(); //find my location
 }
 
 function getMyLocation() {
@@ -22,7 +21,9 @@ function getMyLocation() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			myLat = position.coords.latitude;
 			myLng = position.coords.longitude;
-			renderMap();
+			addMe(); //render map
+			getCarsLocations(); // find all drivers
+			addMe(); // fix the shortest distance to a driver
 		});
 	}
 	else {
@@ -30,20 +31,21 @@ function getMyLocation() {
 	}
 }
 
-function renderMap() {
+function addMe() {
 	me = new google.maps.LatLng(myLat, myLng);
 
 	// Update map and go there...
 	map.panTo(me);
 	
 	// Create a marker
-	marker = new google.maps.Marker({
+	var marker = new google.maps.Marker({
 		position: me,
-		title: "G1cLpMu7B2 is " +  " miles to driver",
+		title: "G1cLpMu7B2 is " + shortDist.toFixed(3) +
+		  " miles to driver",
 		icon: 'icon.jpg'
 	});
 	marker.setMap(map);
-		
+	
 	// Open info window on click of marker
 	google.maps.event.addListener(marker, 'click', function() {
 		infowindow.setContent(marker.title);
@@ -52,36 +54,45 @@ function renderMap() {
 }
 
 function getCarsLocations() {
-	var request;
-	request = new XMLHttpRequest();
+	// Step 1: Make a new XHR
+	var request = new XMLHttpRequest();
 
-	request.open("GET", "https://jordan-marsh.herokuapp.com/rides", true);
+	// Step 2: Open XHR
+	request.open("POST", "https://jordan-marsh.herokuapp.com/rides", false);
 
+    // Step 3: Set the request header to be returned
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    // Step 4: Obtain .json data and work with the data  
 	request.onreadystatechange = function() {
   		if (request.readyState == 4 && request.status == 200) {
 	        var result = request.responseText;
 	     	data = JSON.parse(result);
-	     	for (cnt = 0; cnt < data.length; cnt++) {
-	     		var lat = data.vehicles.lat;
-	     		var lng = data.vehicles.lng
-	     		drivers[cnt].username = data.vehicles[cnt].username; 
+	     	for (cnt = 0; cnt < data.vehicles.length; cnt++) {
+	     		var lat = data.vehicles[cnt].lat;
+	     		var lng = data.vehicles[cnt].lng
+	     		var usrname = data.vehicles[cnt].username; 
 	     		var car = new google.maps.LatLng(lat, lng);
-	     		var dist = google.maps.geometry.spherical.computeDistanceBetween(me, car);
-	     		drivers[cnt].distance = dist;
-	     		addToMap(drivers[cnt].username, drivers[cnt].distance, lat, lng);
+	     		var dist = google.maps.geometry.spherical.computeDistanceBetween(me, car)
+	     		           * 0.000621371;
+	     		if (dist < shortDist)
+	     			shortDist = dist;
+	     		addToMap(usrname, dist, lat, lng); // add driver to the API
 	     	}	
      	}
   	}
-  	//request.send();
+  	
+  	// Step 5: Send my information to the server
+  	request.send("username=G1cLpMu7B2&lat=" + myLat + "&lng=" + myLng);
 }
 
 function addToMap(name, dist, lat, lng) {
 	car = new google.maps.LatLng(lat, lng);
-
+	
 	// Create a marker
-	marker = new google.maps.Marker({
+	var marker = new google.maps.Marker({
 		position: car,
-		title: name + " is " + dist * 0.000621371 +  " miles to passenger",
+		title: "" + name + " is " + dist.toFixed(3) +  " miles to passenger",
 		icon: 'car.png'
 	});
 	marker.setMap(map);
